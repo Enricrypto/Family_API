338a90a7e8edb42b018d2570234368119d1dc682
Enricrypto <eibarraf@gmail.com>
2023-04-20 14:09:25 +0000

    first commit

--- src/datastructures.py
+++ src/datastructures.py
@@ -8,6 +8,13 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+class Member:
+    def __init__(self, first_name, age, lucky_numbers):
+        self.first_name = first_name
+        self.age = age
+        self.lucky_numbers = lucky_numbers
+        self.id = None
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
@@ -20,16 +27,24 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member['last_name'] = self.last_name
+        if id not in member:
+            member['id'] = self._generateId()
+
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for i in range(len(self._members)):
+            if self._members[i]['id'] == id:
+                aux = self._members[i]
+                del self._members[i]
+                return aux
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
